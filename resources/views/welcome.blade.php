3f22affd4616377d55ee5fd5bd68355e42792192
neemchand <neemchand@ucreate.co.in>
2018-07-26 19:28:03 +0530

    text changed on home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Test me laravel
                 </div>
 
                 <div class="links">
